d5ccc536875a0f2e1e663742cd0ca2617d835c5b
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2023-03-26 13:12:56 +0200

    Add lots of implementations
    I can't remember all of them

--- games-battleship/src/main/java/battleship/Gameboard.java
+++ games-battleship/src/main/java/battleship/Gameboard.java
@@ -5,6 +5,7 @@ public class Gameboard {
     public final String IS_MISSED_MESSAGE = "Missed!";
     public final String IS_HIT_MESSAGE = "Hit!";
     public final String IS_SINKED_MESSAGE = "Sinked!";
+    private String fireResultMessage = "No fire was intented";
     private Cell[][] grid;
 
     public Gameboard() {
@@ -19,22 +20,39 @@ public class Gameboard {
         }
     }
 
+    public void setBattleShipAt(Position cellPosition) {
+        int x = cellPosition.GetHorizontalPosition();
+        int y = cellPosition.GetVerticalPosition();
+        grid[x][y] = Cell.BATTLESHIP;
+    }
+    
+    public void setWaterAt(Position cellPosition) {
+        int x = cellPosition.GetHorizontalPosition();
+        int y = cellPosition.GetVerticalPosition();
+        grid[x][y] = Cell.WATER;
+    }
+
+
+
     public void fireAt(Position cellPosition) {
         if (isWithinRange(cellPosition)) {
-            fireAtGameboardCell(cellPosition);
+            fireAtGameboardCell(cellPosition); //TODO: find a better name, somehow
         } else {
             throwCellOutOfRangeException(cellPosition);
         }
     }
 
     private void fireAtGameboardCell(Position cellPosition) {
-        
+        if (isBattleshipAt(cellPosition)) {
+            updateFireResultMessage(cellPosition);
+            hitAt(cellPosition); //TODO: side effect
+        }
     }
 
     private void throwCellOutOfRangeException(Position cellPosition) {
         String message = String.format("The provided cell of position (%d %d) is out of range",
-                                    cellPosition.GetHorizontalPosition(),
-                                    cellPosition.GetVerticalPosition());
+                                       cellPosition.GetHorizontalPosition(),
+                                       cellPosition.GetVerticalPosition());
         throw new IllegalArgumentException(message);
     }
 
@@ -43,5 +61,32 @@ public class Gameboard {
         int y = cellPosition.GetVerticalPosition();
         return (x >= 0) && (x < SIZE) 
                && (y >= 0) && (y < SIZE);
+    }
+
+    private void updateFireResultMessage(Position cellPosition) {
+        if (isLastBattleshipPartAt(cellPosition)) {
+            fireResultMessage = IS_SINKED_MESSAGE;
+        } else if (isBattleshipAt(cellPosition)) {
+            fireResultMessage = IS_HIT_MESSAGE;
+        } else {
+            fireResultMessage = IS_SINKED_MESSAGE;
+        }
+    }
+
+    public boolean isBattleshipAt(Position cellPosition) {
+        int x = cellPosition.GetHorizontalPosition();
+        int y = cellPosition.GetVerticalPosition();
+        return grid[x][y] == Cell.BATTLESHIP;
+    }
+
+    private boolean isLastBattleshipPartAt(Position cellPosition) {
+        int
+        return grid[]
+    }
+
+    private void hitAt(Position cellPosition) {
+        int x = cellPosition.GetHorizontalPosition();
+        int y = cellPosition.GetVerticalPosition();
+        grid[x][y] = Cell.WATER;
     }
 }

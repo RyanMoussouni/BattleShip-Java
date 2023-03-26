69be32eb5cea60cb571ceae01cbc485422c79a87
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2023-03-26 11:03:39 +0200

    Add position

--- games-battleship/src/main/java/battleship/Position.java
+++ games-battleship/src/main/java/battleship/Position.java
@@ -1,4 +1,25 @@
 package battleship;
 
 public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+    }
+
+    public void SetHorizontalPosition(int x) {
+        this.x = x;
+    }
+
+    public int GetHorizontalPosition() {
+        return x;
+    }
+
+    public void SetVerticalPosition(int y) {
+        this.y = y;
+    }
+
+    public int GetVerticalPosition() {
+        return y;
+    }
 }

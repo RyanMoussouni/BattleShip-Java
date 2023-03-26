781630a97bbac3eb1b0b038b0b968f67031c0f9c
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2023-03-26 13:32:12 +0200

    Add getSurroudingPositions body

--- games-battleship/src/main/java/battleship/Position.java
+++ games-battleship/src/main/java/battleship/Position.java
@@ -1,5 +1,7 @@
 package battleship;
 
+import java.security.NoSuchAlgorithmException;
+
 public class Position {
     private int x;
     private int y;
@@ -21,5 +23,39 @@ public class Position {
 
     public int GetVerticalPosition() {
         return y;
+    }
+
+    public Position[] getSurroundingPositions(int[] xLimits, int[] yLimits) {
+        if (isTopLeftCorner(x, y, xLimits, yLimits)) {
+            throw new NoSuchAlgorithmException();
+        } else if (isTopRightCorner(x, y, xLimits, yLimits) ) {
+            throw new NoSuchAlgorithmException();
+        } else if (isDownLeftCorner(x, y, xLimits, yLimits)) {
+            throw new NoSuchAlgorithmException();
+        } else if (isDownRightCorner(x, y, xLimits, yLimits)) {
+            throw new NoSuchAlgorithmException();
+        } else {
+            throw new NoSuchAlgorithmException();
+        }
+
+    }
+
+    private static boolean isTopLeftCorner(int x, int y, int[] xLimits, int[] yLimits) {
+        return x < xLimits[1]  
+            && x > xLimits[0]
+            && y < yLimits[1]
+            && y > yLimits[1];
+    }
+
+    private static boolean isTopRightCorner(int x, int y, int[] xLimits, int[] yLimits) {
+        return true;
+    }
+
+    private static boolean isDownLeftCorner(int x, int y, int[] xLimits, int[] yLimits) {
+        return true;
+    }
+
+    private static boolean isDownRightCorner(int x, int y, int[] xLimits, int[] yLimits) {
+        return true;
     }
 }

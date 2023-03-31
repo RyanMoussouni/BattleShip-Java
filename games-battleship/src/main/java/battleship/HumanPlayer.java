e7d7ec04e0d0bfd10b11642218c7f1f3213fd87e
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2023-03-31 08:25:33 +0200

    Add HumanPlayer behaviour

--- games-battleship/src/main/java/battleship/HumanPlayer.java
+++ games-battleship/src/main/java/battleship/HumanPlayer.java
@@ -1,5 +1,58 @@
 package battleship;
 
-public class HumanPlayer implements Player {
-    
+import java.text.ParseException;
+import java.util.Scanner;
+
+public class HumanPlayer extends AbstractPlayer {
+    private final Scanner scanner;
+    private static final int ASCII_ENCODING_ZERO = 30;
+    private static final int ASCII_ENCODING_NINE = 39;
+
+    public HumanPlayer() {
+        super();
+        scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public Position selectTarget() {
+        var userInputLine = fetchUserInputLine();
+        return tryParseUserInputLine(userInputLine);
+    }
+
+    private Position tryParseUserInputLine(String line) {
+        try {
+            return parseUserInputLine(line);
+        } catch (ParseException e) { //TODO: find a better exception to use
+            handleInvalidFormat();
+        }
+    }
+
+    private Position parseUserInputLine(String line) throws ParseException {
+        if (!isTwoDigitNumber(line)) {
+            throw new ParseException("Unable to read input: you did not provide a two digit number.", 0);
+        } else {
+            int firstDigit = Integer.parseInt(line.substring(0, 1));
+            int secondDigit = Integer.parseInt(line.substring(1,2));
+            return new Position(firstDigit, secondDigit);
+        }
+    }
+
+    private boolean isTwoDigitNumber(String line) {
+        var isOfLengthTwo = line.length() == 2;
+        var firstCharacter = line.charAt(0);
+        var secondCharacter = line.charAt(1);
+        return isOfLengthTwo
+            && isDigit(firstCharacter)
+            && isDigit(secondCharacter);
+    }
+
+    private static boolean isDigit(char c) {
+        var encoding = encodeToASCII(c);
+        return encoding <= ASCII_ENCODING_NINE
+            && encoding >= ASCII_ENCODING_ZERO;
+    }
+
+    private static int encodeToASCII(char c) {
+        return c;
+    }
 }

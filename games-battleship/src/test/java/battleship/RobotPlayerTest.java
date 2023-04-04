a1e19d12880fbbf86da183f8e2ef9fba8f7b0d32
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2023-04-04 20:20:04 +0200

    Add robot player test

---
+++ games-battleship/src/test/java/battleship/RobotPlayerTest.java
@@ -0,0 +1,35 @@
+package battleship;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Before;
+
+public class RobotPlayerTest {
+
+    //TODO: this test is not that clean; is it really what we want
+    @Test
+    public void selectPosition_should_return_no3equalTargets_when_called_3times() {
+        // Arrange
+        var player = new RobotPlayer();
+        var target1 = player.selectTarget();
+        var target2 = player.selectTarget();
+        var target3 = player.selectTarget();
+
+        // Act
+
+        // Assert
+        Assert.assertFalse(target1.equals(target2)
+                                 && target2.equals(target3));
+    }
+
+    @Test
+    public void isDefeated_should_return_true_when_noBattleship() {
+        // Arrange  // should be filled with water, so no battleship
+        var player = new RobotPlayer();
+
+        // Act
+
+        // Assert
+        Assert.assertTrue(player.isDefeated());
+    }
+}

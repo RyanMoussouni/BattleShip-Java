ccd02acad3d38cb39d8461677eb9b7e24145092f
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2023-04-05 08:00:24 +0200

    [WIP] Add player battleship setting

--- games-battleship/src/main/java/battleship/HumanPlayer.java
+++ games-battleship/src/main/java/battleship/HumanPlayer.java
@@ -1,6 +1,7 @@
 package battleship;
 
 import java.text.ParseException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class HumanPlayer extends AbstractPlayer {
@@ -15,29 +16,63 @@ public class HumanPlayer extends AbstractPlayer {
 
     @Override
     public void placeBattleships() {
+        trySelectBattleship(5);
+        trySelectBattleship(4);
+        trySelectBattleship(3);
+        trySelectBattleship(3);
+        trySelectBattleship(2);
+    }
 
+    private void trySelectBattleship(int length) {
+        try {
+            selectBattleship(length);
+        } catch (ParseException pe) {
+            handleInvalidFormat();
+        } catch (InputMismatchException ime) {
+            handleInputOutOfBounds();
+        }
+    }
+
+    private void selectBattleship(int length) {
+        var userInputLine = fetchUserInputLine();
+        var battleshipPosition = tryParseUserBattleshipInputLine(userInputLine);
+        board.setBattleship(battleshipPosition);
     }
 
     @Override
     public Position selectTarget() {
         var userInputLine = fetchUserInputLine();
-        return tryParseUserInputLine(userInputLine);
+        return tryParseUserTargetInputLine(userInputLine);
     }
 
     private String fetchUserInputLine() {
         return scanner.nextLine();
     }
 
-    private Position tryParseUserInputLine(String line) {
+    private Position[] tryParseUserBattleshipInputLine(String line) {
         try {
-            return parseUserInputLine(line);
+            return parseUserBattleshipInputLine(line);
+        } catch (ParseException e) {
+            handleInvalidFormat();
+            return new Position[] { new Position(0, 0) };
+        }
+    }
+
+    private Position[] parseUserBattleshipInputLine(String line) {
+        return new Position[] { new Position(5,5)};
+    }
+
+    //TODO: refactor this
+    private Position tryParseUserTargetInputLine(String line) {
+        try {
+            return parseUserTargetInputLine(line);
         } catch (ParseException e) {
             handleInvalidFormat();
             return new Position(0,0); //TODO: remove this
         }
     }
 
-    private Position parseUserInputLine(String line) throws ParseException {
+    private Position parseUserTargetInputLine(String line) throws ParseException {
         if (!isTwoDigitNumber(line)) {
             throw new ParseException("Unable to read input: you did not provide a two digit number.", 0);
         } else {
